a3d93967c5eb03f4dc314dfe43291a524865f3c0
Guy Aridor <garidor@gmail.com>
2016-11-19 16:14:24 -0500

    correct for missing -1 in un

--- macro/replication/rep.m
+++ macro/replication/rep.m
@@ -18,7 +18,7 @@
  kss_denom = 1 + delta * beta + delta * beta^2 + delta * beta^3 + (delta - 1) * beta^4;
  kss = (theta * (4 * beta^4) / kss_denom) * yss^(v+1) * ((yss)^(-v) - sigma * (zss)^(-v));
  nss_first_term = ((1 - theta) * (yss)^(v+1)*((yss)^(-v) - sigma*(zss)^(-v)))^(-1);
- nss_second_term = (1 - 1/mu) * (yss - delta * kss);
+ nss_second_term = (1/mu - 1) * (yss - delta * kss);
  nss = (nss_first_term * nss_second_term + 1)^-1;
  css = yss - delta * kss;
  
