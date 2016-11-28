ea46b1f585a04067dd230b7b7c54703a3ec7f5ad
Guy Aridor <garidor@gmail.com>
2016-11-28 18:04:27 -0500

    finalize some stuff

--- math-methods/ps9/ps9.jl
+++ math-methods/ps9/ps9.jl
@@ -38,6 +38,13 @@ function pick_next_state(probs::Array{Float64, 1})
     return sample(get_states(probs), wn)
 end
 
+is_high_return_state(current_state::Int64) = current_state == 1 || current_state == 3;
+is_low_return_state(current_state::Int64) = current_state == 2 || current_state == 4;
+is_high_liquidity_state(current_state::Int64) = current_state == 1 || current_state == 2;
+is_low_liquidity_state(current_state::Int64) = current_state == 3 || current_state == 4;
+high_return_result(cur::Float64) = cur * 0.06 + cur;
+low_return_result(cur::Float64) = cur * -0.20 + cur;
+
 function iterate_markov_chain(mc::MarkovChain, num_iter::Int64)
     current_state = find_initial_state(mc);
     state_history = [current_state];
@@ -63,27 +70,64 @@ long_run_no_coffee = find_stationary_distribution(MarkovChain(sick_no_coffee));
 long_run_coffee = find_stationary_distribution(MarkovChain(sick_with_coffee));
 
 
-function run_analysis_for_three(mc::MarkovChain, num_simulations::Int64, T::Int64)
-    num_time_in_low_liquidity = 0
-    num_time_in_low_return = 0
-    total_num_periods = T * num_simulations
+function run_analysis_for_four(mc::MarkovChain, num_simulations::Int64, T::Int64)
+    results = simulate(mc, num_simulations, T);
+    return analyze_results(results, num_simulations, T);
+end
+
+function simulate(mc::MarkovChain, num_simulations::Int64, T::Int64)
     results = []
     for i=1:num_simulations
         result = iterate_markov_chain(mc, T);
-        append!(results, result)
-        num_time_in_low_liquidity += size(filter(x -> x == true, map(x -> x == 3 || x == 4, result)))[1]
-        num_time_in_low_return += size(filter(x -> x == true, map(x -> x == 2 || x == 4, result)))[1]
+        append!(results, [result])
+    end
+    return results
+end
+
+function analyze_results(results, num_simulations::Int64, T::Int64)
+    amount_histories = [];
+    liquidity_histories = [];
+    num_time_in_low_liquidity = 0
+    num_time_in_low_return = 0
+    total_num_periods = T * num_simulations
+    for result in results
+        amount_history = [];
+        liquidity_history = [];
+        amount = 100.0;
+        for state in result
+            append!(amount_history, copy(amount));
+            if is_high_return_state(state)
+                amount = high_return_result(amount);
+            else is_low_return_state(state)
+                num_time_in_low_return += 1;
+                amount = low_return_result(amount);
+            end
+            if is_high_liquidity_state(state)
+                append!(liquidity_history, 1);
+            else is_low_liquidity_state(state)
+                num_time_in_low_liquidity+= 1;
+                append!(liquidity_history, 0);
+            end
+        end
+        append!(liquidity_histories, [liquidity_history]);
+        append!(amount_histories, [amount_history]);
     end
     fraction_of_time_in_low_liquidity = num_time_in_low_liquidity / total_num_periods
     fraction_of_time_in_low_return = num_time_in_low_return / total_num_periods
-    return (fraction_of_time_in_low_return, fraction_of_time_in_low_liquidity)
+    return (amount_histories, liquidity_histories, fraction_of_time_in_low_return, fraction_of_time_in_low_liquidity)
 end
 
 P_a = [.9603 .0297 .0097 .0003; .891 .099 .009 .001; .125 .125 .375 .375; .025 .225 .075 .675];
 pi = [1 0 0 0];
 st_b = MarkovChain(P_a, pi);
-res_b = run_analysis_for_three(st_b, 4, 100)
+res_b = run_analysis_for_four(st_b, 4, 100);
+long_run_return_b = run_analysis_for_four(st_b, 4, 100000);
+long_run_low_return_b = long_run_return_b[3];
+long_run_low_liquidity_b = long_run_return_b[4];
 
 P_c = [.9603 .0297 .0097 .0003; .9603 .0297 .0097 .0003; .005 .005 .495 .495; .005 .005 .495 .495];
 st_c = MarkovChain(P_c, pi);
-res_c = run_analysis_for_three(st_c, 4, 100)
+res_c = run_analysis_for_four(st_c, 4, 100);
+long_run_return_c = run_analysis_for_four(st_c, 4, 100000);
+long_run_low_return_c = long_run_return_c[3];
+long_run_low_liquidity_c = long_run_return_c[4];
