1019f1db8564ca962c4651f85f7aa5bc2764030c
Guy Aridor <garidor@gmail.com>
2016-12-03 22:51:26 -0500

    metrics pset9 code

---
+++ metrics/ps9.R
@@ -0,0 +1,8 @@
+ar1.sim<-arima.sim(model=list(ar=c(.7)),n=200)
+ts.plot(ar1.sim)
+ar.acf<-acf(ar1.sim,type="correlation",plot=T)
+
+
+arma.sim<-arima.sim(model=list(ar=c(.7,-.3),ma=c(.8,.4)),n=200)
+ts.plot(arma.sim)
+arma.acf<-acf(arma.sim,type="correlation",plot=T)
