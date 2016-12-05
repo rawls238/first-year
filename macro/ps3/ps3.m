f3b45e48d9d1ca2bc82d4a78a3373023235af457
Guy Aridor <garidor@gmail.com>
2016-12-05 16:53:13 -0500

    ps3

--- macro/ps3/ps3.m
+++ macro/ps3/ps3.m
@@ -1,3 +1,6 @@
+clear all;
+close all;
+
 [dat, headers] = xlsread('CEE1999data.xls');
 vals = zeros(1, length(headers));
 for i=1:length(vals)
@@ -9,25 +12,79 @@ T = length(dat(:,vars('ENTRY')));
 VARn = 7;
 VARp = 4;
 
-z = [dat(:,vars('RFF'))'; dat(:,vars('TR'))'; dat(:,vars('NBR'))'; dat(:,vars('M1'))'; dat(:,vars('Y'))'; dat(:,vars('P'))'; dat(:,vars('Pc'))'];
-Z = [lagmatrix(z',1) lagmatrix(z',2) lagmatrix(z',3) lagmatrix(z',4)];
+z = [dat(:,vars('M1'))'; dat(:,vars('Y'))'; dat(:,vars('RFF'))'; dat(:,vars('TR'))'; dat(:,vars('NBR'))'; dat(:,vars('Pc'))'; dat(:,vars('P'))'];
+Z = [lagmatrix(z',1) lagmatrix(z',2) lagmatrix(z',3) lagmatrix(z',4);];
 
 z = z(:,VARp+1:length(z));
 
 Z = Z(VARp+1:length(Z),:)';
-Z = [ones(1,length(Z));Z];
 
 % GLS estimator
 beta  = kron(inv(Z*Z')*Z,eye(VARn))*z(:);
 Sigma = (T-VARn*VARp-1)^(-1)*z*(eye(length(z))-Z'*inv((Z*Z'))*Z)*z';
 
-A0 = beta(1:7);
-A1 = reshape(beta(8:56),7,7);
-A2 = reshape(beta(57:105),7,7);
-A3 = reshape(beta(106:154),7,7);
-A4 = reshape(beta(155:203),7,7);
+A1 = reshape(beta(1:49),7,7);
+A2 = reshape(beta(50:98),7,7);
+A3 = reshape(beta(99:147),7,7);
+A4 = reshape(beta(148:196),7,7);
 
 % Long-run impact matrices
-Phi  = inv(eye(VARn)-A1-A2-A3-A4);
-Phie = (chol(Phi*Sigma*Phi'))'; % Estimated identified long run impact matrix:
-D    = inv(Phi)*Phie;           % Estimated contemporaneous impact matrix:
+D=chol(Sigma);
+
+IRdiffRFF(:,5)  = D(:,3)*[1;0;0;0;0;0;0]';
+IRlevelRFF(:,5) = D(:,3)*[1;0;0;0;0;0;0]';
+for i = 6:19     
+    IRdiffRFF(:,i) = A1*IRdiffRFF(:,i-1)+A2*IRdiffRFF(:,i-2)+A3*IRdiffRFF(:,i-3)+A4*IRdiffRFF(:,i-4);     
+    IRlevelRFF(:,i)= IRlevelRFF(:,i-1)+IRdiffRFF(:,i);
+end
+
+b = figure();
+subplot(4,2,2)
+plot((IRlevelRFF(5,5:19)))
+title('Output: RFF')
+ylabel('Percent')
+%xlabel('Quarters')
+subplot(4,2,4)
+plot(IRlevelRFF(7,5:19))
+title('Pcom: RFF')
+ylabel('Percent')
+%xlabel('Quarters')
+subplot(4,2,6)
+plot(IRlevelRFF(4,5:19)*100)
+xlabel('Quarters')
+ylabel('Percent')
+title('M1: RFF')
+subplot(4,2,8)
+plot((IRlevelRFF(1,5:19)))
+title('Fed funds: RFF')
+ylabel('Percent')
+hold off
+
+IRdiffNBR(:,5)  = D*[0;0;D(5,5);0;0;0;0];
+IRlevelNBR(:,5) = D*[0;0;D(5,5);0;0;0;0];
+for i = 6:19    
+    IRdiffNBR(:,i) = A1*IRdiffNBR(:,i-1)+A2*IRdiffNBR(:,i-2)+A3*IRdiffNBR(:,i-3)+A4*IRdiffNBR(:,i-4);     
+    IRlevelNBR(:,i)= IRlevelNBR(:,i-1)+IRdiffNBR(:,i);
+end
+
+c = figure();
+subplot(4,2,2)
+plot((IRlevelNBR(5,5:19)))
+title('Output: NBR')
+ylabel('Percent')
+%xlabel('Quarters')
+subplot(4,2,4)
+plot(IRlevelNBR(7,5:19))
+title('Pcom: NBR')
+ylabel('Percent')
+%xlabel('Quarters')
+subplot(4,2,6)
+plot(IRlevelNBR(4,5:19)*100)
+xlabel('Quarters')
+ylabel('Percent')
+title('M1: NBR')
+subplot(4,2,8)
+plot((IRlevelNBR(1,5:19)))
+title('Fed funds: NBR')
+ylabel('Percent')
+hold off
