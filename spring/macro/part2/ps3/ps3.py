0ae82c40b077899776cb3555b53b797d1782082b
Guy Aridor <garidor@gmail.com>
2017-04-26 13:54:52 -0400

    updating with p

--- spring/macro/part2/ps3/ps3.py
+++ spring/macro/part2/ps3/ps3.py
@@ -7,17 +7,18 @@ def mean(L):
 
 def calc_equity_premium(p, q, sigma, rho, theta, b):
 	first = sigma*sigma * theta
-	expect = mean(map(lambda x: (1 - x)**(-theta), b))
-	second = p * (1 - q) * expect
-	third = mean(map(lambda x: (1 - x)**(1-theta), b))
+	second= mean(map(lambda x: pow(1-x, -1*theta), b))
+	third = mean(map(lambda x: pow(1-x, 1-theta), b))
 	fourth = mean(b)
-	return first + second - third - fourth
+	return first + p*(1-q)*(second - third - fourth)
 
 
 df = pandas.read_excel('/Users/garidor/Desktop/first-year/spring/macro/part2/ps3/DataForPS3.xlsx')
 col = "% fall in real per capita GDP"
 b = df[col].values
 p = 0.017
+n = len(b)
+approx_num_observations = n/p
 q = 0.4
 gamma = 0.025
 sigma = 0.02
@@ -28,9 +29,12 @@ print(calc_equity_premium(p, q, sigma, rho, theta, b))
 for theta in range(1, 7):
 	vals.append(calc_equity_premium(p, q, sigma, rho, theta, b))
 plt.plot([1, 2, 3, 4, 5, 6], vals)
-#plt.show()
+plt.show()
 
-b = heapq.nsmallest((len(b) - 6), b)
+p = (n - 6.0) / (approx_num_observations - 6.0)
+b = heapq.nsmallest((n - 6), b)
 print(calc_equity_premium(p, q, sigma, rho, theta, b))
-b = heapq.nlargest((len(b) - 30), b)
+
+p = (n - 30.0) / (approx_num_observations - 30.0)
+b = heapq.nlargest((n - 30), b)
 print(calc_equity_premium(p, q, sigma, rho, theta, b))
